d716c9a1276aa00b77195989dddc674e670fd7bd
ide03088 <ide03088@naver.com>
2019-07-04 15:46:32 +0900

    3+5 기능 추가

--- A_FristJavaProgram.java
+++ A_FristJavaProgram.java
@@ -4,6 +4,7 @@ public class A_FristJavaProgram {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java");
 		System.out.println("Frist java program");
+		System.out.println(3+5);
 	}
 
 }
