9c5cb5fd2d6147860db7a725b2bd4764de1c8772
ide03088 <ide03088@naver.com>
2019-07-04 16:09:43 +0900

    주석 추가

--- A_FristJavaProgram.java
+++ A_FristJavaProgram.java
@@ -2,7 +2,9 @@
 public class A_FristJavaProgram {
 
 	public static void main(String[] args) {
+
 		// 한글 테스트 2
+
 		System.out.println("환영합니다 Java");
 		System.out.println("Frist java program");
 		System.out.println(3+5);
