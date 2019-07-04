12145dd42cd1b0994a60edc8c607da0047fe6290
ide03088 <ide03088@naver.com>
2019-07-04 15:44:54 +0900

    한글 출력

--- A_FristJavaProgram.java
+++ A_FristJavaProgram.java
@@ -3,7 +3,7 @@ public class A_FristJavaProgram {
 
 	public static void main(String[] args) {
 		// 한글 테스트 
-		System.out.println("Welcome to Java");
+		System.out.println("환영합니다 Java");
 		System.out.println("Frist java program");
 	}
 
